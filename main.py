da94550ca72600c3729b45286ba83b71d6245faf
zMyrddin <janmarvin.consul@gmail.com>
2023-05-07 14:45:04 +1000

    added task class

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import csv
 import datetime
+
+class Task:
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+        self.done = False
+
+    def mark_done(self):
+        self.done = True
