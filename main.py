dddf8902cc450147539ba3e0e5fe3980317df34e
zMyrddin <janmarvin.consul@gmail.com>
2023-05-07 14:47:10 +1000

    added class dailytracker part 2

--- main.py
+++ main.py
@@ -29,3 +29,29 @@ class Task:
         self.daily_scores.append((datetime.date.today(), earned_points, total_points))
         self.save_tasks_to_csv('tasks.csv')
         self.save_scores_to_csv('daily_scores.csv')
+
+    def display_scores(self):
+        if not self.daily_scores:
+            print("No scores available yet.")
+        else:
+            date, earned_points, total_points = self.daily_scores[-1]
+            print(f"{date}: {earned_points}/{total_points}")
+
+
+    def load_scores_from_csv(self, filename):
+        try:
+            with open(filename, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    date = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
+                    earned_points = int(row[1])
+                    total_points = int(row[2])
+                    self.daily_scores.append((date, earned_points, total_points))
+        except FileNotFoundError:
+            print(f"No '{filename}' file found. A new file will be created when the day is saved.")
+
+    def save_scores_to_csv(self, filename):
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for date, earned_points, total_points in self.daily_scores:
+                writer.writerow([date, earned_points, total_points])
